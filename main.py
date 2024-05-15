98fbd01fb29d185e5b57c05dd458d553ba4a9964
Ownerlost <159863419+Ownerlost@users.noreply.github.com>
2024-05-15 23:02:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Config(object):
     for i in range(len(AUTH_USERS)):
         AUTH_USERS[i] = int(AUTH_USERS[i])
 
-    GROUPS = os.environ.get('GROUPS', '-1002123112478,-4289550908').split(',')
+    GROUPS = os.environ.get('GROUPS', '-1002028071138').split(',')
     for i in range(len(GROUPS)):
         GROUPS[i] = int(GROUPS[i])
 
